ae71e44747721a0632aa765d8f223fb5640ea110
Saulo <sauloluisprofissional@gmail.com>
2025-12-10 21:57:47 -0300

    Adicionando estrutura projeto

---
+++ src/main/java/com/picpaySimplificado/picpaysimplificado/PicpaysimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.picpaySimplificado.picpaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaysimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysimplificadoApplication.class, args);
+	}
+
+}
